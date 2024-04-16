c59f2eb64af67da500050aa7d22f25b9324e8ba6
sharmapreyrit26 <d3preyrit@gmail.com>
2024-04-17 00:57:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+fetch( 'https://icanhazdadjoke.com/slack' )
+.then(data => data.json())
+.then(jokesData => {
+    const jokeText = jokesData.attachments[0].text; 
+    const jokeElement = document.getElementById('jokeElement');
+
+      
+    jokeElement.innerHTML = jokeText;
+})
